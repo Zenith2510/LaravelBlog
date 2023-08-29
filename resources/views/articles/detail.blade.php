7a4f7db5e22f106ff47a32cc372220dc17364428
Zenith2510 <ylay0857@gmail.com>
2023-08-29 23:26:00 +0630

    changes made to select box to place category of article category as default in edit

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -1,70 +1,70 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="container" style="max-width: 800px">
-        @if(session('info'))
-            <div class="alert alert-info">
-                {{ session('info') }}
+<div class="container" style="max-width: 800px">
+    @if(session('info'))
+    <div class="alert alert-info">
+        {{ session('info') }}
+    </div>
+    @endif
+    <div class="card mb-2 border-primary" style="font-size: 1.3em">
+        <div class="card-body">
+            <h3 class="card-title">{{ $article->title }}</h3>
+            <div style="font-size: 0.8em" class="text-muted">
+                <b class="text-success"> {{$article->user->name}} </b>
+                Category: <b>{{ $article->category->name }}</b>
+                {{ $article->created_at->diffForHumans() }}
+
             </div>
-        @endif
-        <div class="card mb-2 border-primary" style="font-size: 1.3em">
-            <div class="card-body">
-                <h3 class="card-title">{{ $article->title }}</h3>
-                <div style="font-size: 0.8em" class="text-muted">
-                    <b class="text-success"> {{$article->user->name}} </b>
-                     Category: <b>{{ $article->category->name }}</b>
-                    {{ $article->created_at->diffForHumans() }}
+            <div>
+                {{ $article->body }}
+            </div>
+            <div class="mt-2">
 
-                </div>
-                <div>
-                    {{ $article->body }}
-                </div>
-                <div class="mt-2">
+                @auth
+                @can('delete-article', $article)
+                <a href="{{ url("/articles/delete/$article->id") }}"
+                    class="btn btn-danger btn-sm">
+                    Delete
+                </a>
+                @endcan
 
-                    @auth
-                        @can('delete-article', $article)
-                            <a href="{{ url("/articles/delete/$article->id") }}"
-                            class="btn btn-danger btn-sm">
-                            Delete
-                        </a>
-                        @endcan
-
-                        @can('edit-article',$article)
-                            <a href="{{url("/articles/update/$article->id")}}"
-                            class="btn btn-primary  btn-sm">Edit</a>
-                        @endcan
-                    @endauth
-                </div>
+                @can('edit-article',$article)
+                <a href="{{url("/articles/update/$article->id")}}"
+                    class="btn btn-primary btn-sm">Edit</a>
+                @endcan
+                @endauth
             </div>
         </div>
-        <ul class="list-group mt-3">
-            <li class="list-group-item active">
-                <b>Comments ({{count($article->comments)}})</b>
-            </li>
-            @foreach ($article->comments as $comment)
-                <li class="list-group-item">
-                    @auth
-                        @can('delete-comment',$comment)
-                            <a href="{{url("/comments/delete/$comment->id")}}" class="btn-close float-end" ></a>
-                        @endcan
-                    @endauth
-                    {{$comment->content}}
-                    <div class="small mt-2">
-                    <b class="text-success">{{ $comment->user->name }}</b>,
-                    {{ $comment->created_at->diffForHumans() }}
-                    </div>
-                </li>
-            @endforeach
-        </ul>
-
-        @auth
-            <form action="{{url('comments/add')}}" method="POST">
-            @csrf
-            <input type="hidden" name="article_id" value="{{$article->id}}">
-            {{-- <input type="hidden" name="user_id" value="{{$comment->user_id}}"> --}}
-            <textarea name="content" class="form-control mb-2" placeholder="New Comment"></textarea>
-            <input type="submit" class="btn btn-secondary" value="Add Comment">
-        </form>
-        @endauth
     </div>
+    <ul class="list-group mt-3">
+        <li class="list-group-item active">
+            <b>Comments ({{count($article->comments)}})</b>
+        </li>
+        @foreach ($article->comments as $comment)
+        <li class="list-group-item">
+            @auth
+            @can('delete-comment',$comment)
+            <a href="{{url("/comments/delete/$comment->id")}}" class="btn-close float-end" ></a>
+            @endcan
+            @endauth
+            {{$comment->content}}
+            <div class="small mt-2">
+                <b class="text-success">{{ $comment->user->name }}</b>,
+                {{ $comment->created_at->diffForHumans() }}
+            </div>
+        </li>
+        @endforeach
+    </ul>
+
+    @auth
+    <form action="{{url('comments/add')}}" method="POST">
+        @csrf
+        <input type="hidden" name="article_id" value="{{$article->id}}">
+        {{-- <input type="hidden" name="user_id" value="{{$comment->user_id}}"> --}}
+        <textarea name="content" class="form-control mb-2" placeholder="New Comment"></textarea>
+        <input type="submit" class="btn btn-secondary" value="Add Comment">
+    </form>
+    @endauth
+</div>
 @endsection
--- resources/views/articles/update.blade.php
+++ resources/views/articles/update.blade.php
@@ -1,37 +1,38 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="container" style="max-width: 600px">
+<div class="container" style="max-width: 600px">
 
-        @if($errors->any())
-            <div class="alert alert-warning">
-                @foreach($errors->all() as $err)
-                    {{ $err }}
-                @endforeach
-            </div>
-        @endif
-
-        <form action="{{url("articles/update/$article->id")}}" method="post">
-            @csrf
-            <div class="mb-2">
-            <input type="hidden" name="article_id" value="{{$article->id}}">
-                <label for="">Title</label>
-                <input type="text" class="form-control" name="title" value="{{$article->title}}">
-            </div>
-            <div class="mb-2">
-                <label for="">Body</label>
-                <textarea name="body" class="form-control">{{$article->body}}</textarea>
-            </div>
-            <div class="mb-3">
-                <label for="">Category</label>
-                <select name="category_id" class="form-select">
-                @foreach ($categories as $category)
-                    <option value="{{$category->id}}">{{$category->name}}</option>
-                 @endforeach
-                </select>
-            </div>
-
-            <button class="btn btn-primary">Add Article</button>
-        </form>
+    @if($errors->any())
+    <div class="alert alert-warning">
+        @foreach($errors->all() as $err)
+        {{ $err }}
+        @endforeach
     </div>
+    @endif
+
+    <form action="{{url("articles/update/$article->id")}}" method="post">
+        @csrf
+        <div class="mb-2">
+            <input type="hidden" name="article_id" value="{{$article->id}}">
+            <label for="">Title</label>
+            <input type="text" class="form-control" name="title" value="{{$article->title}}">
+        </div>
+        <div class="mb-2">
+            <label for="">Body</label>
+            <textarea name="body" class="form-control">{{$article->body}}</textarea>
+        </div>
+        <div class="mb-3">
+            <label for="">Category</label>
+            <select name="category_id" class="form-select">
+                @foreach ($categories as $category)
+                <option value="{{ $category->id }}" {{ $category->id === $article->category_id ? 'selected' : '' }}>
+                    {{ $category->name }}
+                </option> @endforeach
+            </select>
+        </div>
+
+        <button class="btn btn-primary">Add Article</button>
+    </form>
+</div>
 @endsection
